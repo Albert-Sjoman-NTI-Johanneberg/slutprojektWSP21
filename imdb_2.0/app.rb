917db61fbf731718b12915debaa21f54e1831293
BaguetteConnoisseur <Albert.sjoman@elev.ga.ntig.se>
2021-04-26 00:09:55 +0200

    04-25 new
    WORK WORK WORK WORK WORK

--- imdb_2.0/app.rb
+++ imdb_2.0/app.rb
@@ -18,21 +18,83 @@ get('/showregister') do
 
 end
 
-get('/users') do
-  Userid = session[:id].to_i
-  user_data = get_user_data(Userid)
-  movie_data = get_user_movie_data(Userid)
-  review_data = get_user_review_data(Userid)
+post('/login') do
+  username = params[:username]
+  password = params[:password]
 
-  slim(:"users/index", locals:{info:user_data, movie:movie_data, review:review_data})
-  #När du hämtar movie data ta med titel m.m från filmen som reviewades
+  result = get_info_user(username)
+
+  pwdigest = result["pwdigest"]
+  id = result["Id"]
+
+  if password_check(pwdigest,password)
+    session[:id] = id
+    redirect("/movies")
+
+  else
+    "FEL LÖSENORD!!"
+  end
+
+end
+
+get('/users') do
+
+  #Validerar om user har tryckt på user iconen innan inlogning
+  userid = session[:id].to_i
+  if userid == 0
+    slim(:"users/login")
+  else
+
+  user_data = get_user_data(userid)
+  movie_data = get_user_movie_data(userid)
+  review_data = get_user_review_data(userid)
+  
+  
+  #Hämtar data om filmerna som userna har gjort en review på
+  reviewed_movie_data = []
+  review_data.each do |rev|
+    temp = rev["MovieId"]
+    reviewed_movie_data << getreviewed_movie_data(temp)
+
+  end
+
+  p reviewed_movie_data
+  slim(:"users/index", locals:{info:user_data, movie:movie_data, review:review_data, movie_review:reviewed_movie_data})
+  end
+end
+
+post("/users/new") do 
+  username = params[:username]
+  password = params[:password]
+  confirm_password = params[:confirm_password]
+  
+  temp = id_from_user(username)
+  
+  if temp.empty?
+    if (password == confirm_password)
+      password_digest = create_user(username,password)
+      redirect("/")
+
+    else
+      redirect("/showregister")
+    end
+  
+  else
+    redirect("/showregister")
+  end
 end
 
 get("/movies") do
-  id = session[:id].to_i
-  result = get_movies_from_db(id)
-  slim(:"movies/index", locals:{movie:result})
+  #Validerar om user har tryckt på imdb iconen innan inlogning
+  userid = session[:id].to_i
+  if userid == 0
+    slim(:"users/login")
+  else
+    rating = medel_rating()
+    result = get_movies_from_db()
 
+    slim(:"movies/index", locals:{movie:result})
+  end
 end
 
 get('/movies/new') do
@@ -49,14 +111,81 @@ end
 post("/movie/new") do
   movie_name = params[:movie_name]
   user_id = session[:id]  
-  description = params[:description]
-  original_filename = params[:file][:filename]
-  file_ending = search_file_ending(original_filename)
+  desc = params[:description]
+  org_filename = params[:file][:filename]
+  file_ending = search_file_ending(org_filename)
   filename = SecureRandom.uuid + file_ending 
   save_path = File.join("./public/img/uploaded_pictures/", filename)
-  db_path = File.join("/img/uploaded_pictures/", filename)
+  dbpath = File.join("/img/uploaded_pictures/", filename)
 
   FileUtils.cp(params[:file][:tempfile], "./public/img/uploaded_pictures/#{filename}")
-  add_movie(movie_name, description, db_path, user_id)
+  add_movie(movie_name, desc, dbpath, user_id)
   redirect("/users")
+end
+
+get('/review/new/:id') do
+  id = params[:id].to_i
+  session[:movieId] = id
+  result = specific_movie(id)
+  slim(:"reviews/new",locals:{movie:result})
+end
+
+post('/review') do
+  title = params[:review_name]
+  desc = params[:description]
+  rating = params[:rating].to_i
+  movieId = session[:movieId]
+  user_id = session[:id] 
+  time = Time.now.to_i
+  create_review(title, desc, rating, movieId, user_id)
+  redirect('/users')
+end
+
+get('/movies/edit/:id') do
+  id = params[:id].to_i
+  result = specific_movie(id)
+  slim(:"movies/edit",locals:{result:result})
+end
+
+get('/users/edit/:id') do
+  id = params[:id].to_i
+  result = user_and_desc(id)
+  slim(:"users/edit",locals:{result:result})
+end
+
+get('/reviews/edit/:id') do
+  id = params[:id].to_i
+  result = review_movie(id)
+  slim(:"reviews/edit",locals:{result:result})
+end
+
+post('/movies/edit/:id') do
+  id = params[:id].to_i
+  titel = params[:titel]
+  desc = params[:desc]
+  update_movie(title,id,desc)
+  redirect('/users')
+
+end
+
+post('/reviews/edit/:id') do
+  id = params[:id].to_i
+  titel = params[:titel]
+  desc = params[:desc]
+  rating = params[:rating]
+  update_review(id,titel,desc,rating)
+  redirect('/users')
+
+end
+
+post('/reviews/edit/delete/:id') do
+  id = params[:id].to_i
+  delete_review(id)
+  redirect('/users')
+end
+
+post('/movies/edit/delete/:id') do
+  id = params[:id].to_i
+  delete_movie(id)
+  redirect('/users')
 end
--- imdb_2.0/model.rb
+++ imdb_2.0/model.rb
@@ -1,64 +1,88 @@
-post('/login') do
-    username = params[:username]
-    password = params[:password]
-  
-    db = SQLite3::Database.new("db/imdb.db")
-    db.results_as_hash = true
-  
-    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
-    
-    pwdigest = result["pwdigest"]
-    id = result["Id"]
-  
-    if BCrypt::Password.new(pwdigest) == password
-      session[:id] = id
-      redirect("/movies")
-  
-    else
-      "FEL LÖSENORD!!"
-    end
-  
-  end
-
-
-
-
-post("/users/new") do 
-  username = params[:username]
-  password = params[:password]
-  confirm_password = params[:confirm_password]
-  
-  db = SQLite3::Database.new("db/imdb.db")
-  temp = db.execute("SELECT Id FROM users WHERE username = ?", username)
-  
-  if temp.empty?
-    if (password == confirm_password)
-      password_digest = BCrypt::Password.create(password)
-      db.execute('INSERT INTO Users (username,pwdigest) VALUES (?,?)', username,password_digest)
-      redirect("/")
-
-    else
-      redirect("/showregister")
-    end
-  
-  else
-    redirect("/showregister")
-  end
-end
-
-
-def get_movies_from_db(id)
+def get_info_user(username)
   db = SQLite3::Database.new("db/imdb.db")
   db.results_as_hash = true
-  result = db.execute("SELECT * FROM movie WHERE UserId = ?", id)
+  result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+end
+
+def password_check(pwdigest,password)
+  BCrypt::Password.new(pwdigest) == password
+end
+
+def create_user(username,password)
+  password_digest = BCrypt::Password.create(password)
+  db = SQLite3::Database.new("db/imdb.db")
+  db.execute('INSERT INTO Users (username,pwdigest) VALUES (?,?)', username,password_digest)
+  password_digest = BCrypt::Password.create(password)
+end
+
+def id_from_user(username)
+  db = SQLite3::Database.new("db/imdb.db")
+  temp = db.execute("SELECT Id FROM users WHERE username = ?", username)
+  return temp
+end
+
+def update_movie(title,id,desc)
+  db = SQLite3::Database.new("db/imdb.db")
+  db.execute('UPDATE movie SET titel = ?, Content = ? WHERE Id = ?',titel,desc,id)
+end
+
+def update_review(id,titel,desc,rating)
+  db = SQLite3::Database.new("db/imdb.db")
+  db.execute('UPDATE review SET Titel = ?, Content = ?, Rating = ? WHERE Id = ?',titel,desc,rating,id)
+end
+
+def delete_review(id)
+  db = SQLite3::Database.new("db/imdb.db")
+  db.execute("DELETE FROM review WHERE Id = ?", id)
+
+end
+
+def delete_movie(id)
+  db = SQLite3::Database.new("db/imdb.db")
+  img_path = db.execute("SELECT img FROM movie WHERE Id = ?", id)
+  if File.exist?("public#{img_path[0][0]}")    
+    File.delete("public#{img_path[0][0]}")
+  end
+
+  db.execute("DELETE FROM movie WHERE Id = ?", id)
+  db.execute("DELETE FROM review WHERE MovieId = ?", id)
+end
+
+def db_conect
+  db = SQLite3::Database.new("db/imdb.db")
+  db.results_as_hash = true
+  return db
+end
+
+def get_movies_from_db()
+  db = SQLite3::Database.new("db/imdb.db")
+  db.results_as_hash = true
+  
+  result = db.execute("SELECT * FROM movie")
 end
 
 def get_user_data(userId);
   db = SQLite3::Database.new("db/imdb.db")
   db.results_as_hash = true
   user_data = db.execute('SELECT username,description From users Where Id = ? ', userId).first
+end
 
-  p user_data
+def review_movie(id)
+  db = SQLite3::Database.new("db/imdb.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM review WHERE Id = ?", id).first
+end
+
+def specific_movie(id)
+  db = SQLite3::Database.new("db/imdb.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM movie WHERE Id = ?",id).first
+end
+
+def user_and_desc(id)
+  db = SQLite3::Database.new("db/imdb.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT username, description FROM users WHERE Id = ?", id).first
 end
 
 def get_user_movie_data(userId);
@@ -66,7 +90,7 @@ def get_user_movie_data(userId);
   db.results_as_hash = true
   user_movie_data = db.execute('SELECT * FROM movie WHERE userId = ?', userId)
   
-  p user_movie_data
+
 end
 
 def get_user_review_data(userId);
@@ -74,7 +98,6 @@ def get_user_review_data(userId);
   db.results_as_hash = true
   user_review_data = db.execute('SELECT * FROM review WHERE userId = ?', userId)
 
-  p user_review_data
 end
 
 def search_file_ending(original_filename)
@@ -89,3 +112,61 @@ def add_movie(movie_name, description, img_path, user_id)
   db.results_as_hash = true
   db.execute("INSERT INTO movie (Titel, Content, img, UserId) VALUES (?,?,?,?)",movie_name,description,img_path,user_id)
 end
+
+def create_review(title, desc, rating, movieId, user_id) 
+  db = SQLite3::Database.new("db/imdb.db")
+  db.results_as_hash = true
+  db.execute("INSERT INTO review (Titel, Content, Rating, MovieId, UserId) VALUES (?,?,?,?,?)",title, desc, rating, movieId, user_id)
+end
+
+def getreviewed_movie_data(temp)
+  userId = session[:id]
+  db = SQLite3::Database.new("db/imdb.db")
+  db.results_as_hash = true
+  reviewmovie_data = db.execute('SELECT * FROM movie WHERE Id = ?', temp)
+  review_data = db.execute('SELECT * FROM review WHERE MovieId = ? AND userId = ?', temp, userId)
+  temp2 = reviewmovie_data + review_data
+
+  return temp2
+end
+
+def medel_rating() 
+  db = SQLite3::Database.new("db/imdb.db")
+  db.results_as_hash = true
+  total = 0.0
+  i = 0
+  movieId = []
+  medel = []
+  movie = get_movies_from_db()
+  movie.each do |movie|
+    movieId << movie["Id"]
+  end
+
+  movieId.each do |movie|
+    all_rating_from_one_movie = db.execute('SELECT Rating FROM review WHERE MovieId = ?', movie) 
+        
+      all_rating_from_one_movie.each do |rating|
+
+        temp = rating["Rating"]
+        total += temp
+        i += 1
+
+      end
+    p movie
+    if i != 0
+      total = total / i
+      total = total.round(1)
+      db.execute('UPDATE movie SET Rating = ? WHERE Id = ?',total,movie)
+
+    else
+      total = 0
+      db.execute('UPDATE movie SET Rating = ? WHERE Id = ?',total,movie)  
+    end
+
+    total = 0
+    i = 0
+
+  end
+end
+
+
